15a4420bbd329a202129b96b1d5e37ee885ebc79
ashanthilochana <ashanthilochana98@gmail.com>
2024-04-13 18:39:31 +0530

    .env and database sql

--- frontend/src/views/branch_manager/views/ViewOrdersView.js
+++ frontend/src/views/branch_manager/views/ViewOrdersView.js
@@ -1,15 +1,67 @@
-import { Row, Col, Table, Card, CardTitle, CardBody } from "reactstrap";
+import { Row, Col, Table, Card, CardTitle, CardBody, CardSubtitle, Form, FormGroup, Input, FormFeedback, Container, Breadcrumb, BreadcrumbItem } from "reactstrap";
 import ViewOrderTable from "../components/ViewOrdersTable";
 
 const ViewOrders = () => {
   return (
-    <Row>
+    <Container>
 
-      <Col lg="12">
-        <ViewOrderTable />
-      </Col>
- 
-    </Row>
+      {/* Breadcrumbs */}
+      <Breadcrumb>
+        <BreadcrumbItem>
+          <a href="#">
+            Home
+          </a>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <a href="#">
+            Order Manamagement
+          </a>
+        </BreadcrumbItem>
+        <BreadcrumbItem active>
+          View Latest Orders
+        </BreadcrumbItem>
+      </Breadcrumb>
+
+      <Card>
+        <Row>
+          <Col lg="8">
+            <CardBody className="align-items-center">
+              <CardTitle tag="h5">Latest Orders</CardTitle>
+              <CardSubtitle className="mb-2 text-muted" tag="h6">
+                Overview of new orders
+              </CardSubtitle>
+            </CardBody>
+          </Col>
+          <Col lg="4">
+            <CardBody>
+              <Row>
+                <Form>
+                  <FormGroup>
+                    <Input
+                      id="district"
+                      name="district"
+                      placeholder="Enter branch district"
+                      type="text"
+                    // value={inputData.district}
+                    // onChange={onChange}
+                    // invalid={validations.district}
+                    // required = {true}
+                    />
+                    <FormFeedback>Enter a valid district</FormFeedback>
+                  </FormGroup>
+                </Form>
+              </Row>
+            </CardBody>
+          </Col>
+        </Row>
+      </Card>
+      <Row>
+        <Col lg="12">
+          <ViewOrderTable />
+        </Col>
+      </Row>
+    </Container>
+
   );
 };
 
