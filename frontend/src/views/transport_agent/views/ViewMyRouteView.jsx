a1a45b71d23431d8ec3b2696552592b81658706f
heshaniC <heshanijayasinghe93@gmail.com>
2024-05-02 15:23:43 +0530

    viewMyRoutes

--- frontend/src/views/transport_agent/views/ViewMyRouteView.jsx
+++ frontend/src/views/transport_agent/views/ViewMyRouteView.jsx
@@ -1,48 +1,70 @@
+import {
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  Input,
+  FormFeedback,
+  Container,
+  Breadcrumb,
+  BreadcrumbItem,
+  Button
+} from "reactstrap";
 
-import { Button, Col, Container, Row, Card, CardBody } from "reactstrap";
 
+import ViewMyRouteTable from "../components/ViewMyRouteTable";
 
-const ViewMyRouteView = () =>{
-
+const ViewOrderDetailsView = () => {
   return (
     <Container>
+
+      {/* Breadcrumb and search bar */}
+      <Form>
+        <Row className="d-flex">
+          <Col lg="8" className="align-content-center">
+            {/* Breadcrumbs */}
+            <Breadcrumb>
+              <BreadcrumbItem>
+                <a href="#">
+                  Home
+                </a>
+              </BreadcrumbItem>
+              <BreadcrumbItem>
+                <a href="#">
+                  TransportAgent Manamagement
+                </a>
+              </BreadcrumbItem>
+              <BreadcrumbItem active>
+                View All Routes
+              </BreadcrumbItem>
+            </Breadcrumb>
+          </Col>
+          <Col lg="3">
+            <FormGroup>
+              <Input
+                className="pt-2 pb-2"
+                id="routes"
+                name="routes"
+                placeholder="Search Routes..."
+                type="text"
+              />
+              <FormFeedback>Enter a valid Branch Name</FormFeedback>
+            </FormGroup>
+          </Col>
+          <Col lg="1">
+            <Button style={{"margin-top" : "3px"}} color="primary">Search</Button>
+          </Col>
+        </Row>
+      </Form>
+
       <Row>
-        <Card>
-          <CardBody>
-            
-          </CardBody>
-        </Card>
-      </Row>
-      <Row>
-        <Col lg="4">
-          <Card>
-            <CardBody>
-              asdasjdas 
-              asda
-            </CardBody>
-          </Card>
-        </Col>
-        <Col lg="4">
-        <Card>
-            <CardBody>
-              asdasjdas 
-              asda
-            
-            </CardBody>
-          </Card>
-        </Col>
-        <Col lg="4">
-        <Card>
-            <CardBody>
-              asdasjdas 
-              asda
-            
-            </CardBody>
-          </Card>
+        <Col lg="12">
+        <ViewMyRouteTable/>
         </Col>
       </Row>
     </Container>
+
   );
 };
 
-export default ViewMyRouteView;
+export default ViewOrderDetailsView;
